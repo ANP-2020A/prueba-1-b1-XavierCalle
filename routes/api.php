ff132833965147274a85752f99f1a9ad300b3f18
XavierCalle <xav.xgca@gmail.com>
2020-07-09 15:56:03 -0500

    #3 Creacion de rutas y controladores

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,20 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
+
+Route::get('products', 'ProductsController@index');
+Route::get('products/{product}', 'ProductsController@show');
+Route::post('products', 'ProductsController@store');
+Route::put('products/{product}', 'ProductsController@update');
+Route::delete('products/{product}', 'ProductsController@delete');
+
+Route::get('customers', 'CustomersController@index');
+Route::get('customers/{customer}', 'CustomersController@show');
+Route::post('customers', 'CustomersController@store');
+Route::put('customers/{customer}', 'CustomersController@update');
+Route::delete('customers/{customer}', 'CustomersController@delete');
+
+
