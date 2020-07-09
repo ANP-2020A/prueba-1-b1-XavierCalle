635dd7888677400f4f31735b0632142408a4e53b
XavierCalle <xav.xgca@gmail.com>
2020-07-09 16:51:46 -0500

    #4 #5 Autenticacion con JWT y proteccion de rutas

--- routes/api.php
+++ routes/api.php
@@ -16,16 +16,32 @@ use Illuminate\Http\Request;
     return $request->user();
 });*/
 
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@authenticate');
 Route::get('products', 'ProductsController@index');
 Route::get('products/{product}', 'ProductsController@show');
-Route::post('products', 'ProductsController@store');
-Route::put('products/{product}', 'ProductsController@update');
-Route::delete('products/{product}', 'ProductsController@delete');
 
-Route::get('customers', 'CustomersController@index');
-Route::get('customers/{customer}', 'CustomersController@show');
-Route::post('customers', 'CustomersController@store');
-Route::put('customers/{customer}', 'CustomersController@update');
-Route::delete('customers/{customer}', 'CustomersController@delete');
+
+
+
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('user', 'UserController@getAuthenticatedUser');
+
+    Route::post('products', 'ProductsController@store');
+    Route::put('products/{product}', 'ProductsController@update');
+    Route::delete('products/{product}', 'ProductsController@delete');
+
+    Route::get('customers', 'CustomersController@index');
+    Route::get('customers/{customer}', 'CustomersController@show');
+    Route::post('customers', 'CustomersController@store');
+    Route::put('customers/{customer}', 'CustomersController@update');
+    Route::delete('customers/{customer}', 'CustomersController@delete');
+
+ });
+
+
+
+
 
 
